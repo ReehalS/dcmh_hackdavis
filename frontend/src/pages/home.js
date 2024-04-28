ec74e13a416a947a9c30c1b47da13700284a8026
SandeepR <sunny.reehal@gmail.com>
2024-04-28 05:07:17 -0700

    Update home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,11 +1,12 @@
 import {Link} from 'react-router-dom';
 
-
+// Add links to other pages
 const Home = () => {
     return (
         <div className="home">
             <h1>Homepage</h1>
             <p>
+            
                 <Link to="/addItem" className='homeLink'>Add Item</Link>
                 <Link to="/itemTable" className='homeLink'>View Items</Link>
                 <Link to="/modifyInventory" className='homeLink'>modifyInventory</Link>
