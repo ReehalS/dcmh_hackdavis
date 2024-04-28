e702ebdbffb383b384da98da1cc384c40d4bc839
k8tlynvo <kqvo@ucdavis.edu>
2024-04-27 21:30:21 -0700

    Revert "email api"

--- server/index.js
+++ server/index.js
@@ -3,17 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors');
 const mongoose = require('mongoose')
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
-
 const userRoutes = require('./routes/user.js')
 const itemRoutes = require('./routes/items.js')
-const emailRoutes = require('./routes/email.js')
 
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(bodyParser.json());
+
+
 
 app.use((req, res, next) => {
   console.log(req.path, req.method)
@@ -22,7 +19,6 @@ app.use((req, res, next) => {
 
 app.use('/api/user', userRoutes)
 app.use('/api/item', itemRoutes)
-app.use('/api/email', emailRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
--- server/routes/email.js
+++
@@ -1,9 +0,0 @@
-const express= require('express')
-
-const { sendMail } = require('../controllers/emailController')
-
-const router = express.Router()
-
-router.post('/text-mail', sendMail); 
-
-module.exports = router;
